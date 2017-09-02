abf7073d02ce495f7ff2d78628ef92d454b37623
Ebram Tawfik <engberosamy@yahoo.com>
2017-09-02 14:12:40 -0700

    "Add" "Edit" and Remove

--- MyProject.WebApplication/Scripts/script.js
+++ MyProject.WebApplication/Scripts/script.js
@@ -28,19 +28,134 @@ MyApp.config(['$routeProvider', function ($routeProvider) {
 
 }]);
 
-MyApp.controller("AddController", function ($scope) {
-    $scope.message = "in Add View";
+MyApp.controller("AddController", function ($scope, StudentAPI) {
+    $scope.addStudent = function () {
+        var studentToAdd = {
+            'S_Name': $scope.S_Name
+        };
+        var onSuccess = function (response) {
+            alert("Student Added");
+            $scope.S_Name = undefined;
+        };
 
+        var onFail = function (reason) {
+            $scope.error = reason;
+            alert("Error in Adding" + reason);
+        };
+
+        var addStudent = StudentAPI.addStudent;
+
+
+        addStudent(studentToAdd).then(onSuccess, onFail);
+
+    };
+   
 });
 
-MyApp.controller("EditController", function ($scope) {
-    $scope.message = "in Edit View";
+MyApp.controller("EditController", function ($scope, StudentAPI) {
 
+    $scope.selectedItem = "Select Student";
+    $scope.isDeleteVisible = false;
+
+    //#region GetStudents
+
+    var onSuccessGetStudents = function (response) {
+        $scope.students = response.data;
+    };
+
+    var onFailGetStudents = function (reason) {
+        $scope.error = reason;
+    };
+
+    var getStudents = StudentAPI.getStudents;
+
+    getStudents().then(onSuccessGetStudents, onFailGetStudents);
+
+    //#endregion GetStudents
+
+
+    $scope.itemSelected = function (sItem) {
+        $scope.selectedItem = sItem.S_Id;
+        $scope.isDeleteVisible = true;
+        $scope.S_Name = sItem.S_Name;
+    };
+
+    $scope.updateStudent = function () {
+        var studentToUpdate = {
+            'S_Id': $scope.selectedItem,
+            'S_Name': $scope.S_Name
+        };
+
+        var onSuccessUpdate = function (response) {
+            alert("Student Updated");
+            $scope.selectedItem = "Select Student";
+            $scope.S_Name = undefined;
+            $scope.isDeleteVisible = false;
+            getStudents().then(onSuccessGetStudents, onFailGetStudents);
+        };
+
+        var onFailUpdate = function (reason) {
+            $scope.error = reason;
+            alert("Error in Updating" + reason);
+        };
+
+        var editStudent = StudentAPI.editStudent;
+
+
+        editStudent(studentToUpdate).then(onSuccessUpdate, onFailUpdate);
+    };
 });
 
-MyApp.controller("DeleteController", function ($scope) {
-    $scope.message = "in Delete View";
+MyApp.controller("DeleteController", function ($scope, StudentAPI) {
+    $scope.selectedItem = "Select Student";
+    $scope.isDeleteVisible = false;
 
+    //#region GetStudents
+
+    var onSuccessGetStudents = function (response) {
+        $scope.students = response.data;
+    };
+
+    var onFailGetStudents = function (reason) {
+        $scope.error = reason;
+    };
+
+    var getStudents = StudentAPI.getStudents;
+
+    getStudents().then(onSuccessGetStudents, onFailGetStudents);
+
+    //#endregion GetStudents
+
+
+    $scope.itemSelected = function (sItem) {
+        $scope.selectedItem = sItem.S_Id;
+        $scope.isDeleteVisible = true;
+        $scope.S_Name = sItem.S_Name;
+    };
+
+    $scope.deleteStudent = function () {
+        var studentToDelete = {
+            'S_Id': $scope.selectedItem
+        };
+
+        var onSuccessDelete = function (response) {
+            alert("Student Deleted");
+            $scope.selectedItem = "Select Student";
+            $scope.S_Name = undefined;
+            $scope.isDeleteVisible = false;
+            getStudents().then(onSuccessGetStudents, onFailGetStudents);
+        };
+
+        var onFailDelete = function (reason) {
+            $scope.error = reason;
+            alert("Error in Delete" + reason);
+        };
+
+        var deleteStudent = StudentAPI.deleteStudent;
+
+
+        deleteStudent(studentToDelete).then(onSuccessDelete, onFailDelete);
+    };
 });
 
 MyApp.controller("HomeController", function ($scope, StudentAPI) {
--- MyProject.WebApplication/service.js
+++ MyProject.WebApplication/service.js
@@ -5,8 +5,28 @@ StudentService.factory('StudentAPI', function ($http) {
     var urlBase = "http://localhost:60793/api/";
     var StudentAPI = {};
     
-    StudentAPI.getStudents = function () {
+    StudentAPI.getStudents = function (student) {
         return $http.get(urlBase+"student");
+    };
+
+
+    StudentAPI.addStudent = function (studentToAdd) {
+        return $http.post(urlBase + "student/", studentToAdd);
+    };
+
+    StudentAPI.editStudent = function (studentToUpdate) {
+        return $http({
+            method: 'put',
+            url: urlBase + "student/" + studentToUpdate.S_Id,
+            data: studentToUpdate
+        });
+    };
+
+    StudentAPI.deleteStudent = function (studentToDelete) {
+        return $http({
+            method: 'delete',
+            url: urlBase + "student/" + studentToDelete.S_Id
+        });
     };
 
     return StudentAPI;
